9654aeb601d1b06b0cccd45b91c30e367c365e79
T7nzIp <62398015+T7nzIp@users.noreply.github.com>
2021-02-06 21:33:13 +0100

    ciao

--- src/com/example/ObsScore/ScoreUpdater.java
+++ src/com/example/ObsScore/ScoreUpdater.java
@@ -487,6 +487,8 @@ public class ScoreUpdater{
             }
         });
 
+
+
         btnResetPunteggio.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
